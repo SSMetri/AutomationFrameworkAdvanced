951c9ea74c35e245c1ce803c2876aa634fb221a8
soubhagya <soubhagyasm@gmail.com>
2023-11-04 10:30:16 +0530

    Commit Browserstack

---
+++ src/test/java/contactsTests/Create_Contact_CROSSBROWSER.java
@@ -0,0 +1,50 @@
+package contactsTests;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import genericUtilities.BaseClass;
+import objectReposatry.ContactDetailsPage;
+import objectReposatry.ContactsPage;
+import objectReposatry.CreateConatctPage;
+import objectReposatry.HomePage;
+
+public class Create_Contact_CROSSBROWSER extends BaseClass {
+	@Test(groups="SmokeTest")
+	public void createContactMethodTest() throws Throwable 
+	{
+		String ORGNAME=eUtil.readDataFromExcelFile("Contacts",7,3)+jUtil.getRandomNumber();
+		String LASTNAME=eUtil.readDataFromExcelFile("Contacts",7,2)+jUtil.getRandomNumber();
+		
+		String INDUSTRY = eUtil.readDataFromExcelFile("Organization",7,3);
+		String TYPE = eUtil.readDataFromExcelFile("Organization",7,4);
+		
+		System.out.println(ORGNAME);
+		
+		//Step 6: Navigate to contacts 
+		HomePage hp=new HomePage(driver);
+		hp.clickOnContactsLink();
+		Reporter.log("Navigate to contacts ");
+		
+		//Step 7: Click on lookup image 
+		ContactsPage cp=new ContactsPage(driver);
+		cp.createContactLookUpIcon();
+		Reporter.log("Click on lookup image");
+		
+		//Step 8: Fill all required details
+		CreateConatctPage cConP=new CreateConatctPage(driver);
+		cConP.creaeNewCOntact(LASTNAME);
+		Reporter.log("Fill all required details in contats page");
+		
+		//Step 9: Verification
+		ContactDetailsPage orgDet=new ContactDetailsPage(driver);
+		String header = orgDet.getHeaderTest();
+		Assert.assertTrue(header.contains(LASTNAME));
+		System.out.println("******END******");
+
+		Reporter.log("Verification, whether contact is created or not");
+		System.out.println("****DONE****");
+	}
+
+}
